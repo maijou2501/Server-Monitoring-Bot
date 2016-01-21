1721fb8822fb32b0c1518db9168fdff36fba94f1
maijou2501 <maijou2501@gmail.com>
2016-01-22 01:30:19 +0900

    Del, files

--- test.rb
+++
@@ -1,9 +0,0 @@
-# webページに任意の文字を出力させる
-# @param [String] 表示させたい文章
-# @return [nil] 引数が Date 型以外の場合は nil
-def print_test(string)
-  print("Hello, ", string , ".\n")
-end
-
-print "Content-Type: text/html\n\n";
-print_test("heyheytower")
