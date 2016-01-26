4946bc34475607e491f542f6c931dc534e185d2f
maijou2501 <maijou2501@gmail.com>
2016-01-27 04:53:52 +0900

    Del, copy missed...

--- src/spec/task_spec.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../lib/task'
-require_relative '../spec/spec_helper.rb'
-
-RSpec.describe Task do
-	it "return nil(CHECK ADDRESS is undifined.)" do
-		expect(Task.new.exec).to eq nil
-	end
-
-end
